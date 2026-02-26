c8e4e985d8a3fc0ede4239cc6d8230ef1bb3975f
Damilare Adegbola <damilarejustin7@gmail.com>
2026-02-26 18:45:33 +0100

    Fixed Database Issues

--- ecopulse_proj/ecopulse_proj/settings.py
+++ ecopulse_proj/ecopulse_proj/settings.py
@@ -80,11 +80,11 @@ WSGI_APPLICATION = 'ecopulse_proj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', '3306'),
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'uaDoBBAlXiChPROTFLfahxRxgSywYjNC',
+        'HOST': 'mysql.railway.internal',
+        'PORT': '3306',
     }
 }
 
