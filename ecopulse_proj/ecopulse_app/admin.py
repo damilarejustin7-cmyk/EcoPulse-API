01803b03fa0e539e4d2769af76c66094f76b43b6
Damilare Adegbola <damilarejustin7@gmail.com>
2026-02-28 12:24:41 +0100

    Fix errors In Admin.py

--- ecopulse_proj/ecopulse_app/admin.py
+++ ecopulse_proj/ecopulse_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import UserProfile, ProjectSite, Resource, EnvironmentalMetric, Allocation, ResourceCategory, MetricType
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(ProjectSite)
+admin.site.register(Resource)
+admin.site.register(EnvironmentalMetric)
+admin.site.register(Allocation)
+admin.site.register(ResourceCategory)
+admin.site.register(MetricType)
